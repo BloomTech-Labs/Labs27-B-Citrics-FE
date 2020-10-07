8a48f889696afef4cd94d67b47f3df6389228d56
nick-ussery <nick.ussery@gmail.com>
2020-10-07 10:53:24 -0500

    map stuff

--- src/components/pages/Home/SearchBar/mapservice.js
+++ src/components/pages/Home/SearchBar/mapservice.js
@@ -1,16 +1,34 @@
 import React, { useEffect } from 'react';
-import google from 'google';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
 let api_key = 'AIzaSyBjf47vw8-reZPcmiv3opmxVgJaFmTpbAE';
 
 function Mapservice() {
+  let map;
+
+  function Map() {
+    return (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 41.4925, lng: 99.9018 }}
+      />
+    );
+  }
+
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
+
   return (
     <>
       <div
-        id="map-container"
-        style={{ border: '1px solid black', height: '300px', width: '300px' }}
+        id="map"
+        style={{ border: '1px solid black', height: '600px', width: '600px' }}
       >
-        map goes here
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${api_key}`}
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '100%' }} />}
+          mapElement={<div style={{ height: '100%' }} />}
+        />
       </div>
     </>
   );
