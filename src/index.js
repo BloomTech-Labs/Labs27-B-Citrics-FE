cc0f9a74e406dcb6a3fff2a6b91158fac0a49a2d
meep-morp <gracieaerogirl@gmail.com>
2020-10-12 11:33:01 -0500

    Added 'exact' to '/' route

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function App() {
         <Route path="/login" component={LoginPage} />
         <Route path="/implicit/callback" component={LoginCallback} />
         {/* any of the routes you need secured should be registered as SecureRoutes */}
-        <Route path="/" component={HomePage} />
+        <Route path="/" exact component={HomePage} />
         <Route path="/map" component={MapService} />
         <Route path="/search" component={SearchBar} />
       </Switch>
