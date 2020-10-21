d54e38f6773db95e5982fcbda32eadb0d2f943e3
Angelg10 <angelguzman97@gmail.com>
2020-10-21 13:14:31 -0400

    added fontawesome icons, city metrics on compare page, and styling to city cards

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 
 import { Layout, Card } from 'antd';
 
+import '../styles/home.css';
+import './FontAwesomeIcons/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const searchCard = {
   display: 'flex',
   flexDirection: 'row',
@@ -13,11 +17,38 @@ const CityCard = props => {
   return (
     <>
       <div className="search-cities" style={searchCard}>
-        <Card bordered={true} style={{ width: 300 }}>
-          <p>{props.name}</p>
-          <p>{props.pop}</p>
-          <p>{props.rent}</p>
-          <p>{props.website}</p>
+        <Card bordered={true} style={{ width: 500, height: 475 }}>
+          <img
+            className="city-img"
+            src={props.wiki_img_url}
+            alt="Wikipedia Img of City"
+            height="200px"
+            width="400px"
+          />
+          <div className="city-metrics">
+            <h2 className="city-name">{`${props.name}, ${props.statename}`}</h2>
+            <p>
+              <FontAwesomeIcon icon={['fas', 'users']}></FontAwesomeIcon>
+              Population: {props.pop}
+            </p>
+            <p>
+              <FontAwesomeIcon icon={['fas', 'house-user']}></FontAwesomeIcon>
+              Average Rent: ${props.rent}
+            </p>
+            <p>
+              <FontAwesomeIcon
+                icon={['fas', 'money-bill-wave']}
+              ></FontAwesomeIcon>
+              Average Househole Income: ${props.household}
+            </p>
+            <p>
+              <FontAwesomeIcon icon={['fas', 'laptop']}></FontAwesomeIcon>
+              City Website:
+              <a href={`${props.website}`} target="_blank">
+                {props.website}
+              </a>
+            </p>
+          </div>
         </Card>
       </div>
     </>
