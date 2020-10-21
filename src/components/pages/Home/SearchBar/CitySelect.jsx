c631da354c0f22678fb0916d7c29e7402a672f24
meep-morp <gracieaerogirl@gmail.com>
2020-10-21 17:19:02 -0500

    Added more data

--- src/components/pages/Home/SearchBar/CitySelect.jsx
+++ src/components/pages/Home/SearchBar/CitySelect.jsx
@@ -20,7 +20,7 @@ const CitySelect = props => {
     return (
         <div className="city-select">
             <div className="compare-list">
-                {props.list.length > 1
+                {props.list.length > 1 && selectedInfo[0]
                     ? <>
                         <h3>Current List</h3>
                         <Carousel
@@ -50,7 +50,7 @@ const CitySelect = props => {
                         className="btn compare">View Comparison</Button>}
             </div>
             <div className="city-info">
-                {props.list.length !== 0 && props.selected
+                {props.list.length !== 0 && props.selected && selectedInfo[0]
                     ? <> <h2>{props.selected.cityName}</h2>
                         <h3>{selectedInfo[0].statename}</h3>
                         <img
@@ -62,6 +62,8 @@ const CitySelect = props => {
                         <h3>About</h3>
                         <p>Time-Zone: {selectedInfo[0].timezone}</p>
                         <p>Population: {selectedInfo[0].pop}</p>
+                        <p>Counties: {selectedInfo[0].counties}</p>
+                        <p>Density: {selectedInfo[0].density_mi_sq} miles, {selectedInfo[0].density_km_sq} kilometers</p>
                     </>
                     : <div style={{
                         textAlign: 'center',
